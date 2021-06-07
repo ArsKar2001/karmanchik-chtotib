5380a9864afdf40085db5b9935aff844c4803ddf
karmanchik <arseniy.karmanov@mail.ru>
2021-06-07 13:31:49 +0900

    com - 1.6.8

--- telegram-bot-start.sh
+++ telegram-bot-start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# shellcheck disable=SC2126
+# shellcheck disable=SC2009
 COUNT_GREP=$(ps -ef | grep telegram-bot.jar | wc -l)
-echo $COUNT_GREP
+echo "$COUNT_GREP"
 if [ "$COUNT_GREP" -ne 2 ] 
 then
-	cd /home/project/karmanchik-chtotib/
+	cd /home/project/karmanchik-chtotib/ || exit
 	java -jar bots/telegram-bot/target/telegram-bot.jar &
 	echo 2
 fi
